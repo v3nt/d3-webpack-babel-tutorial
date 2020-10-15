db2889af22d67cf89e8575d799dd425c57b7c164
v3nt <dan@jynk.net>
2020-10-15 16:27:00 +0100

    dev config start

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 import * as d3 from "d3";
 
 const square = d3.selectAll("rect");
-square.style("fill", "orange");
+square.style("fill", "red");
---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: { main: ["./src/app.js"] },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My App",
+      template: "./src/index.html",
+    }),
+  ],
+};
+
+// loaders: [
+//       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+//       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+//     ]
